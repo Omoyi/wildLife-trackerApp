6479eb7d1e50739993e263cfea8d29c860255505
Omoyi <urakhel7@gmail.com>
2019-09-24 11:53:05 +0200

    create a file

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -72,7 +72,7 @@ public class App {
    // route when clicking on "Add Animal to System"
     get("/animal/new", (request, response) -> {
       Map<String, Object> model = new HashMap<String, Object>();
-      model.put("template", "templates/animal-form.vtl");
+      model.put("template", "/animal-form.vtl");
       return new ModelAndView(model, layout);
     }, new HandlebarsTemplateEngine());
 
